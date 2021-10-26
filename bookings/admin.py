0866ae53bda9ac92ba2d029149d54d36cd227e4e
Jonathan Nzete <jonathannzete@gmail.com>
2021-10-26 09:40:34 +0100

    created BookingDetail and BookedRoom model

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from .models import Booking
+from .models import BookedRoom, Booking, BookingDetail
 
 class BookingAdmin(admin.ModelAdmin):
     madel = Booking
     list_display = ['room', 'start_date', 'duration' ,'end_date', 'customer', 'status']
+
+class BookingDetailInline(admin.TabularInline):
+    model = BookedRoom
+    extra = 0
+    readonly_fields = ['created', 'room', 'customer', 'start_date', 'duration', 'end_date']
+
+class BookingDetailAdmin(admin.ModelAdmin):
+    model = BookingDetail
+    list_dispay = ['room', 'customer']
+    inlines = [BookingDetailInline, ]
 
 admin.site.register(Booking, BookingAdmin)
--- bookings/models.py
+++ bookings/models.py
@@ -29,3 +29,50 @@ class Booking(models.Model):
 
     def __str__(self):
         return self.room.room_number
+
+
+class Payment(models.Model):
+    payment_id = models.CharField(max_length=100)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, related_name='payment')
+    amount = models.DecimalField(decimal_places=2, max_digits=20)
+    payment_method = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+    created = models.DateTimeField()
+
+    def __str__(self):
+        return self.customer
+
+
+class BookingDetail(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    total_book = models.IntegerField()
+    is_booked = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.customer
+
+
+class BookedRoom(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    booking_detail = models.ForeignKey(BookingDetail, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, related_name='booked_room')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    cost = models.DecimalField(decimal_places=2, max_digits=20)
+    start_date = models.DateField()
+    duration = models.IntegerField()
+    end_date = models.DateField()
+    created = models.DateTimeField()
+
+    def __str__(self):
+        return self.room
