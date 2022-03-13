5bf44f1698a07c0382cd8656f874a5f0fc877e69
Jonathan Nzete <jonathannzete@gmail.com>
2022-03-13 07:30:32 +0100

    dumped data and changed database to sqlite3

--- config/settings.py
+++ config/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.str('DATABASE_NAME'),
-        'USER': env.str('DATABASE_USER'),
-        'PASSWORD': env.str('DATABASE_PASSWORD'),
-        'HOST': env.str('DATABASE_HOST'),
-        'PORT': env.str('DATABASE_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
